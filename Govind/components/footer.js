e3a82bd8cac7c65b9ddf6bd7ab7e7d8b19051b7d
GovindLakhotiya <99141948+Govindlakhotiya@users.noreply.github.com>
2022-04-01 20:04:49 +0530

    Add files via upload

---
+++ Govind/components/footer.js
@@ -0,0 +1,50 @@
+const footer = () => {
+  return ` 
+    <div id="solution">
+                <h2>GET TO KNOW US</h2>
+                <p><a href="">ABOUT US</a></p>
+                <p><a href="">CONTACT US</a></p>
+                <p><a href="">EARTHCARE</a></p>
+                <p><a href="">AFFILIATE PROGRAM</a></p>
+                <p><a href="">TERMS & CONDITION</a></p>
+                <p><a href="">REFUND & CANCELLATION</a></p>
+                <p><a href="">PRIVACY POLICY</a></p>
+                <p><a href=""> ORDER & SHIPPING</a></p>
+               
+                <p><a href="">PAYMENT TERMS</a></p>
+                <p><a href="">TERMS OF SERVICE</a></p>
+                <p><a href="">REFUND POLICY</a></p>
+
+            </div>
+            <!-- platform column -->
+            <div id="platform">
+                <h2>ALSO AVAILABLE ON</h2>
+                <p><a href="https://www.nykaa.com">NYKAA</a></p>
+                <p><a href="https://www.amazon.in/">AMAZON</a></p>
+                <p><a href="https://www.flipkart.com/">FLIPKART</a></p>
+                <p><a href="https://www.myntra.com/">MYNTRA</a></p>
+                <p><a href="https://www.foxy.in/brands/dot-key">FOXY</a></p>
+                
+            </div>
+            <!-- company column -->
+            <div id="company">
+                <h2>STAY UPDATED</h2>
+             <input type="email" value placeholder="email@example.com" name="email"/>  
+             <button type="submit">SUBMIT</button>
+
+        <div>
+            <p>FOLLOW US</p>
+            <div class="icon">
+                <a href="https://business.facebook.com/dotandkey"><i class="fa-brands fa-facebook-f "></i></a>
+                <a href="https://www.instagram.com/dotandkey.skincare/"><i class="fa-brands fa-instagram"></i> </a>
+               <a href="https://www.youtube.com/channel/UCD7U0EUBYg9YlL-zVZjkjfA"><i class="fa-brands fa-youtube"></i></a>
+            </div>
+        </div>
+           
+        <p>© 2022 Dot & Key</p>
+    </div>
+`
+
+}
+
+export default footer;
